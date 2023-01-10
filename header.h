063a11018ec1756b63e23dba7735136b03aacc90
johny <jsavard@c2r6p8.42quebec.com>
2023-01-10 13:26:53 -0500

    validation map OK

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:24:13 by jsavard           #+#    #+#             */
-/*   Updated: 2023/01/09 17:04:19 by jsavard          ###   ########.fr       */
+/*   Updated: 2023/01/10 13:03:47 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,13 @@
 #include "MLX42/include/MLX42/MLX42.h"
 #include "get_next_line/get_next_line.h"
 #include "Libft/libft.h"
+
+typedef struct s_game
+{
+	int	map_row;
+	int	map_col;
+	int	map_player;
+	int	map_collectible;
+	int	map_exit;
+	int	map_walkable;
+}t_game;
--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:00:05 by jsavard           #+#    #+#             */
-/*   Updated: 2023/01/09 17:08:14 by jsavard          ###   ########.fr       */
+/*   Updated: 2023/01/10 13:21:29 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-#define WIDTH 256
-#define HEIGHT 256
 
 mlx_image_t	*g_img;
 
@@ -33,22 +31,159 @@ void	hook(void *param)
 		g_img->instances[0].x += 5;
 }
 
-int	check_map(char *map_file)
+int	map_row(char *map_file)
 {
-	int	len_name;
+	int		i;
+	int		fd;
+	char	*temp;
+
+	i = 0;
+	fd = open(map_file, O_RDONLY);
+	do
+	{
+		temp = get_next_line(fd);
+		if (temp)
+		{
+			i++;
+		}
+	}
+	while (temp);
+	free (temp);
+	close(fd);
+	return (i);
+}
+
+int	validate_map(char *line, int row, t_game *game)
+{
+	int	i;
+
+	i = 0;
+	if (row == 1)
+	{
+		while (i < ft_strlen(line) - 1)
+		{
+			if (line[i] != '1')
+			{
+				return (0);
+			}
+			i++;
+		}
+		return (1);
+	}
+	else if (row == game->map_row)
+	{
+		while (i < ft_strlen(line))
+		{
+			if (line[i] != '1')
+			{
+				return (0);
+			}
+			i++;
+		}
+		return (1);
+	}
+	else
+	{
+		if (game->map_col == ft_strlen(line))
+		{
+			while (i < ft_strlen(line) - 1)
+			{
+				if (line[i] == 'P')
+				{
+					game->map_player++;
+					if (game->map_player > 1)
+					{
+						return (0);
+					}
+				}
+				else if (line[i] == 'E')
+				{
+					game->map_exit++;
+					if (game->map_exit > 1)
+					{
+						return (0);
+					}
+				}
+				else if (line[i] == 'C')
+				{
+					game->map_collectible++;
+				}
+				else if (line[i] == '0')
+				{
+					game->map_walkable++;
+				}
+				else if (line[i] == '1')
+				{
+					
+				}
+				else
+				{
+					return (0);
+				}
+				i++;
+			}
+			if (line[0] == '1' && line[game->map_col - 2] == '1')
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
+
+int	read_map(char *map_file, t_game *game)
+{
+	int		i;
+	int		fd;
+	char	*temp;
+
+	i = 0;
+	fd = open(map_file, O_RDONLY);
+	game->map_row = map_row(map_file);
+	do
+	{
+		temp = get_next_line(fd);
+		if (temp)
+		{
+			i++;
+			ft_putnbr_fd(i, 1);
+			ft_putstr_fd(" - ", 1);
+			ft_putstr_fd(temp, 1);
+			if (i == 1)
+			{
+				game->map_col = ft_strlen(temp);
+			}
+			if (validate_map(temp, i, game) == 0)
+			{
+				ft_putchar_fd('X', 1);
+				return (0);
+			}
+		}
+	}
+	while (temp);
+	free (temp);
+	close(fd);
+	return (i);
+}
+
+int	check_map(char *map_file, t_game *game)
+{
+	int		len_name;
 
 	len_name = ft_strlen(map_file);
 	if (map_file[len_name - 1] == 'r' && map_file[len_name - 2] == 'e'
-		&& map_file[len_name - 1] == 'b' && map_file[len_name - 1] == '.')
+		&& map_file[len_name - 3] == 'b' && map_file[len_name - 4] == '.')
 	{
-		return (1);
-		//Check file existe
-		//Open File Get_next_line
-		//Check if Rectangle
-		//Check if has player
-		//Check if has collectible
-		//Check if has exit
-		//Check if has external wall
+		if (read_map(map_file, game) != 0)
+		{
+			ft_putnbr_fd(game->map_collectible, 1);
+			ft_putnbr_fd(game->map_player, 1);
+			if (game->map_collectible > 0 && game->map_walkable > 1
+			&& game->map_player == 1 && game->map_exit == 1)
+			{
+				return (1);
+			}
+		}
 		//Check if can finish
 	}
 	return (0);
@@ -56,16 +191,17 @@ int	check_map(char *map_file)
 
 int32_t	main(int argc, char **argv)
 {
+	t_game	game;
 	mlx_t	*mlx;
 
 	if (argc == 2)
 	{
-		if (check_map(argv[1]) != 0)
+		if (check_map(argv[1], &game) != 0)
 		{
-			mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+			mlx = mlx_init((64 * game.map_col), (64 * (game.map_row + 1)), "MLX42", true);
 			if (!mlx)
 				exit(EXIT_FAILURE);
-			g_img = mlx_new_image(mlx, 128, 128);
+			g_img = mlx_new_image(mlx, 64, 64);
 			memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
 			mlx_image_to_window(mlx, g_img, 0, 0);
 			mlx_loop_hook(mlx, &hook, mlx);
