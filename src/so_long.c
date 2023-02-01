aaf9dcbcd991d0e52f76eeb631f33dc80e7e0af3
johny <jsavard@c3r1p4.42quebec.com>
2023-02-01 18:04:46 -0500

    Leaks patch free game

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:00:05 by jsavard           #+#    #+#             */
-/*   Updated: 2023/01/27 10:52:48 by jsavard          ###   ########.fr       */
+/*   Updated: 2023/02/01 17:59:32 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ int	main(int argc, char **argv)
 	}
 	else
 		ft_putstr_fd("Error\nMauvais parametres!", 1);
-	free_game(&game);
+	if (argc == 2)
+		free_game(&game);
 	return (0);
 }
